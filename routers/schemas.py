731ac42bac3b340aa59e8ad985b1d561a665876f
Farida004 <farida_alv04@yahoo.com>
2022-08-09 23:16:23 +0400

    user authentication post deletion

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class UserBase(BaseModel):
     username:str
@@ -10,3 +11,31 @@ class UserDisplay(BaseModel):
     email:str
     class Config():
         orm_mode = True
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str # relative (if upload image to API ) or absolute (image from internet)
+    caption: str
+    creator_id: int
+
+# Only for post display 
+class User(BaseModel):
+    username:str
+    class Config():
+        orm_mode = True
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str 
+    caption: str
+    timestamp = datetime
+    user : User
+    class Config():
+        orm_mode = True
+
+class UserAuth(BaseModel):
+    id: int
+    username:str
+    email:str
+
